2b1fed8616f4c9a972edf60ab370286ed95d3ec1
codedis213 <codedish213@gmail.com>
2015-06-26 15:31:33 +0530

    form handling

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,13 +1,4 @@
-# import webapp2
-#
-# class MainPage(webapp2.RequestHandler):
-#     def get(self):
-#         self.response.headers['Content-Type'] = 'text/plain'
-#         self.response.out.write('Hello, webapp2 World!')
-#
-# application = webapp2.WSGIApplication([
-#     ('/', MainPage)
-# ], debug=True)
+import cgi
 
 from google.appengine.api import users
 import webapp2
@@ -17,14 +8,38 @@ class MainPage(webapp2.RequestHandler):
         user = users.get_current_user()
 
         if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello, ' + user.nickname())
+            # self.response.headers['Content-Type'] = 'text/plain'
+            # self.response.out.write('Hello, ' + user.nickname())
+            self.response.out.write("""
+              <html>
+                <body>
+                  <form action="/sign" method="post">
+                    <div><textarea name="content" rows="3" cols="60"></textarea></div>
+                    <div><input type="submit" value="Sign Guestbook"></div>
+                  </form>
+                </body>
+              </html>""")
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
+
+
+
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.out.write('<html><body>You wrote:<pre>')
+        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write('</pre></body></html>')
+
+
+
 application = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),
+    ('/sign', Guestbook)
 ], debug=True)
+
+
 
 def main():
     application.run()

ca5cd574c1bd0af87227ec11983812cfd1ed53f7
codedis213 <codedish213@gmail.com>
2015-06-26 15:13:46 +0530

    hello app  with wsgi class Requesthandle and Request responce

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,3 +1,10 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello, world!'
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp2 World!')
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
